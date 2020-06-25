9561bac04d32c50c035696a395f825af8c7239b9
hstrada <h.strada@hotmail.com>
2020-06-25 09:24:42 -0300

    feat: create new calculator structure

--- js/scripts.js
+++ js/scripts.js
@@ -1,90 +1,71 @@
-var inputsDinheiro = document.querySelectorAll(".dinheiro");
-inputsDinheiro.forEach(function(input) {
-    input.addEventListener("keypress", mascaraDinheiro);
-});
-
-var spanHora = document.querySelector(".secao-hora .secao__rodape__valor span");
-var formularioHora = document.querySelector(".secao-hora form");
-formularioHora.addEventListener("change", calculaValorHora);
-
-
-var spanProjeto = document.querySelector(".secao-projeto .secao__rodape__valor span");
-var formProjeto = document.querySelector(".secao-projeto form");
-formProjeto.addEventListener("change", calculaValorProjeto);
-
-
 function converteDinheiro(valor) {
-    return valor.replace(/[\D]+/g, "");
+  return valor.replace(/[\D]+/g, "");
 }
-
 
 function formataDinheiro(valor) {
-    valor = parseInt(converteDinheiro(valor)) + "";
-    valor = valor.replace(/([0-9]{2})$/g, ".$1");
-    if (valor.length > 6) {
-        valor = valor.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
-    }
-    return valor;
+  valor = parseInt(converteDinheiro(valor)) + "";
+  valor = valor.replace(/([0-9]{2})$/g, ".$1");
+  if (valor.length > 6) {
+    valor = valor.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
+  }
+  return valor;
 }
-
 
 function mascaraDinheiro() {
-    var input = this;
-    var valor = input.value;
-    input.value = formataDinheiro(valor);
+  var input = this;
+  var valor = input.value;
+  input.value = formataDinheiro(valor);
 }
 
+var formularioHora = document.querySelector(".secao-hora form");
+var spanHora = document.querySelector(".secao-hora .secao__rodape__valor span");
 
 function calculaValorHora() {
-    // entrada
-    var formHora = this;
-    var ganhoPorMes = converteDinheiro(formHora["ganho-mes"].value);
-    var horasPorDia = parseFloat(formHora["horas-dia"].value);
-    var diasPorSemana = parseFloat(formHora["dias-semana"].value);
-    var semanasDeFerias = parseFloat(formHora["semanas-ferias"].value);
+  // entrada
+  var formHora = this;
+  var ganhoPorMes = converteDinheiro(formHora["ganho-mes"].value);
+  var horasPorDia = parseFloat(formHora["horas-dia"].value);
+  var diasPorMes = parseFloat(formHora["dias-mes"].value);
 
-    // processamento
-    var horasPorSemana = horasPorDia * diasPorSemana;
-    var horasDeFerias = horasPorSemana * semanasDeFerias;
-    var totalHoras = horasPorSemana - horasDeFerias;
-    var ganhoPorAno = ganhoPorMes * 12;
-    var valorDeHora = ganhoPorAno / (52.1 * totalHoras); // divide o ganho anual por 52.1% das horas totais
-    valorDeHora += 0.2 * valorDeHora; // soma 20% do valor da hora
-    valorDeHora = parseFloat(valorDeHora).toFixed(2);
+  var qtdTotalDeHoras = horasPorDia * diasPorMes;
 
-    var valorDeHoraFormatado;
-    if (!isNaN(valorDeHora) && isFinite(valorDeHora)) {
-        valorDeHoraFormatado = Math.ceil(valorDeHora) + ",00";
-    } else {
-        valorDeHoraFormatado = "0,00";
-    }
+  var valorDeHora = parseFloat(ganhoPorMes / qtdTotalDeHoras).toFixed(2);
 
-    formProjeto["ganho-hora"].value = valorDeHoraFormatado;
-    calculaValorProjeto();
+  console.log(valorDeHora);
 
-    // saida
-    spanHora.innerHTML = "R$ " + valorDeHoraFormatado;
+  var valorDeHoraFormatado;
+  if (!isNaN(valorDeHora) && isFinite(valorDeHora)) {
+    valorDeHoraFormatado = Math.ceil(valorDeHora) + ",00";
+  } else {
+    valorDeHoraFormatado = "0,00";
+  }
+
+  console.log(valorDeHoraFormatado);
+
+  // saida
+  spanHora.innerHTML = "R$ " + valorDeHoraFormatado;
 }
 
+formularioHora.addEventListener("change", calculaValorHora);
 
-function calculaValorProjeto() {
-    // entrada
-    var formProjeto = this;
-    var ganhoHora = parseFloat(converteDinheiro(formProjeto["ganho-hora"].value));
-    var projetoHoras = parseFloat(formProjeto["projeto-horas"].value);
-    var projetoDias = parseFloat(formProjeto["projeto-dias"].value);
+// function calculaValorProjeto() {
+//   // entrada
+//   var formProjeto = this;
+//   var ganhoHora = parseFloat(converteDinheiro(formProjeto["ganho-hora"].value));
+//   var projetoHoras = parseFloat(formProjeto["projeto-horas"].value);
+//   var projetoDias = parseFloat(formProjeto["projeto-dias"].value);
 
-    // processamento
-    var valorDeProjeto = ganhoHora * projetoHoras * projetoDias;
-    valorDeProjeto = parseFloat(valorDeProjeto).toFixed(2);
-    
-    var valorDeProjetoFormatado;
-    if (!isNaN(valorDeProjeto) && isFinite(valorDeProjeto)) {
-        valorDeProjetoFormatado = "R$ " + Math.ceil(valorDeProjeto) + ",00";
-    } else {
-        valorDeProjetoFormatado = "R$ 0,00";
-    }
+//   // processamento
+//   var valorDeProjeto = ganhoHora * projetoHoras * projetoDias;
+//   valorDeProjeto = parseFloat(valorDeProjeto).toFixed(2);
 
-    // saida
-    spanProjeto.innerHTML = valorDeProjetoFormatado;
-}
+//   var valorDeProjetoFormatado;
+//   if (!isNaN(valorDeProjeto) && isFinite(valorDeProjeto)) {
+//     valorDeProjetoFormatado = "R$ " + Math.ceil(valorDeProjeto) + ",00";
+//   } else {
+//     valorDeProjetoFormatado = "R$ 0,00";
+//   }
+
+//   // saida
+//   spanProjeto.innerHTML = valorDeProjetoFormatado;
+// }
